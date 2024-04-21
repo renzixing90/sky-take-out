dde2ebca31ce961f81678012d827cf4f3c6f163f
renzixing90 <wutijiangnan@qq.com>
2024-04-21 22:53:44 +0800

    新增菜品业务代码开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,5 +1,8 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
+import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -15,4 +18,11 @@ public interface DishMapper {
     @Select("select count(id) from dish where category_id = #{categoryId}")
     Integer countByCategoryId(Long categoryId);
 
+    /**
+     * 插入菜品
+     *
+     * @param dish
+     */
+    @AutoFill(OperationType.INSERT)
+    void insert(Dish dish);
 }
---
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -0,0 +1,14 @@
+package com.sky.service;
+
+import com.sky.dto.DishDTO;
+
+public interface DishService {
+
+    /**
+     * 新增菜品
+     *
+     * @param dishDTO
+     */
+    void saveWithFlavor(DishDTO dishDTO);
+
+}
