3c6691d2f77e321b535f7c4c47b90f6909254428
renzixing90 <wutijiangnan@qq.com>
2024-04-24 15:17:19 +0800

    商品浏览业务功能代码开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -62,4 +62,12 @@ public interface DishService {
      * @return
      */
     List<Dish> list(Long categoryId);
+
+    /**
+     * 条件查询菜品和口味
+     *
+     * @param dish
+     * @return
+     */
+    List<DishVO> listWithFlavor(Dish dish);
 }
