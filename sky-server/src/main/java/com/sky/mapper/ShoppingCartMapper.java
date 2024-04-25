291c3ecf5172b1ec83246d7cce1fe21b283be99d
renzixing90 <wutijiangnan@qq.com>
2024-04-25 12:49:49 +0800

    地址簿功能代码

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -38,8 +38,17 @@ public interface ShoppingCartMapper {
 
     /**
      * 根据用户id删除购物车数据
+     *
      * @param userId
      */
     @Delete("delete from shopping_cart where user_id = #{userId}")
     void deleteByUserId(Long userId);
+
+    /**
+     * 根据id删除购物车数据
+     *
+     * @param id
+     */
+    @Delete("delete from shopping_cart where id = #{id}")
+    void deleteById(Long id);
 }
--- sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -16,6 +16,7 @@ public interface ShoppingCartService {
 
     /**
      * 查看购物车
+     *
      * @return
      */
     List<ShoppingCart> showShoppingCart();
@@ -24,4 +25,11 @@ public interface ShoppingCartService {
      * 清空购物车
      */
     void cleanShoppingCart();
+
+    /**
+     * 删除购物车中一个商品
+     *
+     * @param shoppingCartDTO
+     */
+    void subShoppingCart(ShoppingCartDTO shoppingCartDTO);
 }
